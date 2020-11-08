a346dd2593f7ef5ca9e0393327707201b517514a
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-08 16:33:15 -0500

    Remove nav component from app.js

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navigation from "./components/Nav";
 import Home from "./components/views/Home";
 import About from "./components/views/About";
 import Projects from "./components/views/Projects";
 import Contact from "./components/views/Contact";
-
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>
-        <Navigation />
         <Route exact path="/" component={Home}/>
         <Route exact path="/about" component={About}/>
         <Route exact path="/projects" component={Projects}/>
