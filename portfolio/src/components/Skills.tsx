66a9084379bb25e119527996b5695b217fcb0ecc
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-08 17:59:35 -0500

    Add about component with child Skills component; add iconify icons for Skills

---
+++ portfolio/src/components/Skills.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Icon, InlineIcon } from '@iconify/react';
+import html5Icon from '@iconify-icons/simple-icons/html5';
+import css3Icon from '@iconify-icons/simple-icons/css3';
+import javascriptIcon from '@iconify-icons/simple-icons/javascript';
+import typescriptIcon from '@iconify-icons/simple-icons/typescript';
+import jqueryIcon from '@iconify-icons/simple-icons/jquery';
+import nodeDotJs from '@iconify-icons/simple-icons/node-dot-js';
+import npmIcon from '@iconify-icons/simple-icons/npm';
+import mysqlIcon from '@iconify-icons/simple-icons/mysql';
+import mongodbIcon from '@iconify-icons/simple-icons/mongodb';
+import githubIcon from '@iconify-icons/simple-icons/github';
+import herokuIcon from '@iconify-icons/simple-icons/heroku';
+
+const Skills = () => {
+  return (
+    <Col md="4" xs="12">
+        <Row className="mr-0">
+          <Col md="3" sm="12">
+            <h2 className="vertical">SKILLS</h2>
+          </Col>
+          <Col md="8" sm="11" className="icon-col">
+            <Icon icon={html5Icon} className="svgicon" />
+            <Icon icon={css3Icon} className="svgicon" />
+            <Icon icon={javascriptIcon} className="svgicon" />
+            <Icon icon={typescriptIcon} className="svgicon" />
+            <Icon icon={jqueryIcon} className="svgicon" />
+            <Icon icon={nodeDotJs} className="svgicon" />
+            <Icon icon={npmIcon} className="svgicon" />
+            <Icon icon={mysqlIcon} className="svgicon" />
+            <Icon icon={mongodbIcon} className="svgicon" />
+            <Icon icon={githubIcon} className="svgicon" />
+            <Icon icon={herokuIcon} className="svgicon" />
+          </Col>
+        </Row>
+      </Col>
+  )
+}
+
+export default Skills;
---
+++ portfolio/src/components/views/About.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Skills from "../Skills";
+
+
+import Navigation from "../Nav";
+
+
+const About = () => (
+  <>
+    <Navigation />
+    <Row className="mr-0">
+      <Col md="3" xs="4" className="pr-0">
+        <img src="./img/flower.png" alt="flower" className="flower-img"></img>
+      </Col>
+      <Col md="5" xs="8">
+        <Row className="first-row">
+          <Col lg="4" className="pic-column">
+            <img src="./img/profilePic1.jpg" alt="profile" className="profile-pic"></img>
+          </Col>
+          <Col lg="8">
+            <p className="p-about">
+              Hello there! My name is Molly and I currently teach high school science in Maine. In anticipation for a future career change, I'm studying full-stack web development through the UNH Coding Boot Camp (along with some Edx and Udemy courses on the side).
+            </p>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col lg="4" className="pic-column">
+            <img src="./img/bodhi.jpg" alt="Bodhi Dog" className="profile-pic"></img>
+          </Col>
+          <Col lg="8">
+            <p className="p-about">
+              In my spare time, I love to hang out with my dog Bodhi - teaching him tricks and taking him for walks on the beach or on the trails. I'm also interested in music, herbal medicine/natural health, yoga, social justice and the interconnectedness of all things.
+            </p>
+          </Col>
+
+        </Row>
+        <hr />
+      </Col>
+      <Skills />
+    </Row>
+  </>
+);
+
+export default About;
