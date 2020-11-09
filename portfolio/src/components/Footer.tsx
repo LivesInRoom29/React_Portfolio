4cd1758c3fc8e7a1a8e0743cc3a37e25ebdb541b
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-08 22:24:54 -0500

    add footer component

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+
+const Footer = () => (
+  <footer className="page-footer">
+    <Row className="footer-copyright mr-0">
+      <h6 className="footer-copyright">Copyright &copy; 2020, Molly Kizer</h6>
+    </Row>
+  </footer>
+);
+
+export default Footer;
