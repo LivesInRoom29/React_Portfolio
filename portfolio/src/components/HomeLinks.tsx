bb11331e91d35dfa43b748d312b007ac23750510
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-09 18:24:12 -0500

    Wrap Button in Link in HomeLinks

--- portfolio/src/components/HomeLinks.tsx
+++ portfolio/src/components/HomeLinks.tsx
@@ -1,24 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
-// import bee from "../assets/img/bee.png";
-// import honeypot from "../assets/img/honeypot.png";
 
 interface HomeLinkProps {
   value: string,
-  imagename: string //not using this at the moment - may try to change images on links
+  imagename: string
 }
 
 const HomeLinks = (props: HomeLinkProps) => {
-  // let image = props.imagename === "honeypot" ? honeypot : bee;
-  // let imgAlt = props.imagename === "honeypot" ? "honeypot" : "bee";
 
   return (
     <div>
-    <Button href={`/${props.value}`} className="home-button">
-      <img src={`./img/${props.imagename}.png`} alt={props.imagename} className="home-img"></img>
-      <h3>{props.value}</h3>
-    </Button>
-  </div>
+      <Link to={`/${props.value}`} className="home-button">
+        <Button className="home-button">
+          <img src={`./img/${props.imagename}.png`} alt={props.imagename} className="home-img"></img>
+          <h3>{props.value}</h3>
+        </Button>
+      </Link>
+
+    </div>
   )
 }
 
