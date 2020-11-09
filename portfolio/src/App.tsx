2e132fbb0f2459fe692f48eff1a63b820887e0cb
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-09 18:02:23 -0500

    add basename and switch to react-router

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/views/Home";
 import About from "./components/views/About";
 import Projects from "./components/views/Projects";
@@ -11,14 +11,17 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/React_Portfolio">
       <div>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/about" component={About}/>
-        <Route exact path="/projects" component={Projects}/>
-        <Route exact path="/contact" component={Contact}/>
-        <Route exact path="/credits" component={Credits}/>
-        <Route exact path="/resume" component={Resume}/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/about" component={About}/>
+          <Route exact path="/projects" component={Projects}/>
+          <Route exact path="/contact" component={Contact}/>
+          <Route exact path="/credits" component={Credits}/>
+          <Route exact path="/resume" component={Resume}/>
+          <Route exact path="*" component={Home}/>
+        </Switch>
       </div>
     </Router>
   )
