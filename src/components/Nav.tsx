642ae4ede44755c951183fa5a20d81967c97f01b
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-10 21:19:57 -0500

    change all Nav.Link to Link with class nav-link inside Nav.Item

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown"
-import { NavItem } from "react-bootstrap";
 
 const Navigation = () => {
   return (
@@ -22,12 +21,17 @@ const Navigation = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Item>
-            <Link to="/About">About</Link>
+            <Link to="/About" className="nav-link">About</Link>
           </Nav.Item>
-
+          <Nav.Item>
+            <Link to="/Projects" className="nav-link">Projects</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="/Contact" className="nav-link">Contact</Link>
+          </Nav.Item>
           {/* <Nav.Link href="/React_Portfolio/About">About</Nav.Link> */}
-          <Nav.Link as={Link} to="/Projects">Projects</Nav.Link>
-          <Nav.Link href="/React_Portfolio/Contact">Contact</Nav.Link>
+          {/* <Nav.Link as={Link} to="/Projects">Projects</Nav.Link>
+          <Nav.Link href="/React_Portfolio/Contact">Contact</Nav.Link> */}
           <NavDropdown title="Learn More" id="basic-nav-dropdown">
             <NavDropdown.Item href="/React_Portfolio/resume">Resume</NavDropdown.Item>
             <NavDropdown.Item href="/React_Portfolio/credits">Credits</NavDropdown.Item>
