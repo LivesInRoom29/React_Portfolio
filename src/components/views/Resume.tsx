1d344712f1791ca4444bf387c6a5bf764b5b185a
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-11-10 20:15:12 -0500

    add resume

--- src/components/views/Resume.tsx
+++ src/components/views/Resume.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Navigation from "../Nav";
+import Footer from "../Footer";
 
 const Resume = () => (
   <>
-    <p></p>
+    <div className="main-container">
+      <Navigation />
+      <h1>Resume</h1>
+      <Container>
+        <Row>
+          <iframe src="https://drive.google.com/file/d/1SUefx5VxAWm_nkzDXKzf5SeBv4XX3L11/preview" title="resume" width="640" height="480"></iframe>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
   </>
 );
 
