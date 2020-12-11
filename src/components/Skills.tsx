b0fefe6f7754b33b9392130b7bca0296e4074e7a
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-12-10 21:59:01 -0500

    Add tooltips to skill icons with react-tooltip

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Icon } from '@iconify/react';
+import ReactTooltip from "react-tooltip";
 import html5Icon from '@iconify-icons/simple-icons/html5';
 import css3Icon from '@iconify-icons/simple-icons/css3';
 import javascriptIcon from '@iconify-icons/simple-icons/javascript';
@@ -16,6 +17,7 @@ import githubIcon from '@iconify-icons/simple-icons/github';
 import herokuIcon from '@iconify-icons/simple-icons/heroku';
 
 const Skills = () => {
+
   return (
     <Col md="4" xs="12" className="skills-col">
         <Row className="mr-0">
@@ -23,18 +25,65 @@ const Skills = () => {
             <h2 className="vertical">SKILLS</h2>
           </Col>
           <Col md="8" sm="11" className="icon-col">
-            <Icon icon={html5Icon} className="svgicon" />
-            <Icon icon={css3Icon} className="svgicon" />
-            <Icon icon={javascriptIcon} className="svgicon" />
-            <Icon icon={typescriptIcon} className="svgicon" />
-            <Icon icon={jqueryIcon} className="svgicon" />
-            <Icon icon={nodeDotJs} className="svgicon" />
-            <Icon icon={npmIcon} className="svgicon" />
-            <Icon icon={mysqlIcon} className="svgicon" />
-            <Icon icon={mongodbIcon} className="svgicon" />
-            <Icon icon={reactIcon} className="svgicon"/>
-            <Icon icon={githubIcon} className="svgicon" />
-            <Icon icon={herokuIcon} className="svgicon" />
+            <Icon icon={html5Icon} className="svgicon" data-tip data-for="html"/>
+            <ReactTooltip id="html" place="bottom" effect="solid">
+              HTML5
+            </ReactTooltip>
+
+            <Icon icon={css3Icon} className="svgicon" data-tip data-for="css"/>
+            <ReactTooltip id="css" place="bottom" effect="solid">
+              CSS3
+            </ReactTooltip>
+
+            <Icon icon={javascriptIcon} className="svgicon" data-tip data-for="js"/>
+            <ReactTooltip id="js" place="bottom" effect="solid">
+              JavaScript
+            </ReactTooltip>
+
+            <Icon icon={typescriptIcon} className="svgicon" data-tip data-for="ts"/>
+            <ReactTooltip id="ts" place="bottom" effect="solid">
+              TypeScript
+            </ReactTooltip>
+
+            <Icon icon={jqueryIcon} className="svgicon" data-tip data-for="jquery"/>
+            <ReactTooltip id="jquery" place="bottom" effect="solid">
+              jQuery
+            </ReactTooltip>
+
+            <Icon icon={nodeDotJs} className="svgicon" data-tip data-for="node"/>
+            <ReactTooltip id="node" place="bottom" effect="solid">
+              Node.js
+            </ReactTooltip>
+
+            <Icon icon={npmIcon} className="svgicon" data-tip data-for="npm"/>
+            <ReactTooltip id="npm" place="bottom" effect="solid">
+              npm
+            </ReactTooltip>
+
+            <Icon icon={mysqlIcon} className="svgicon" data-tip data-for="mysql"/>
+            <ReactTooltip id="mysql" place="bottom" effect="solid">
+              MySQL
+            </ReactTooltip>
+
+            <Icon icon={mongodbIcon} className="svgicon" data-tip data-for="mongo"/>
+            <ReactTooltip id="mongo" place="bottom" effect="solid">
+              MongoDB
+            </ReactTooltip>
+
+            <Icon icon={reactIcon} className="svgicon" data-tip data-for="react"/>
+            <ReactTooltip id="react" place="bottom" effect="solid">
+              React
+            </ReactTooltip>
+
+            <Icon icon={githubIcon} className="svgicon" data-tip data-for="github"/>
+            <ReactTooltip id="github" place="bottom" effect="solid">
+              GitHub
+            </ReactTooltip>
+
+            <Icon icon={herokuIcon} className="svgicon" data-tip data-for="heroku"/>
+            <ReactTooltip id="heroku" place="bottom" effect="solid">
+              Heroku
+            </ReactTooltip>
           </Col>
         </Row>
       </Col>
