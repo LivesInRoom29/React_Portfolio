245b4c2c203539d0ef7f09e81d52e8977e369c69
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-12-19 15:26:43 -0500

    fix link to deployed Daily Planner

--- src/assets/projectData.tsx
+++ src/assets/projectData.tsx
@@ -58,7 +58,7 @@ const projectData = [
     type: "individual",
     name: "Daily Planner",
     repo: "https://github.com/LivesInRoom29/Daily_Planner",
-    deployed: "https://github.com/LivesInRoom29/Daily_Planner",
+    deployed: "https://livesinroom29.github.io/Weather_Dashboard/",
     image: "./img/plannerScreenShot.jpg",
     description: "A daily calendar that provides a way for the user to plan their work day; saves calendar input in local storage.",
     techUsed: "HTML, JavaScript, jQuery, Moment.js",
