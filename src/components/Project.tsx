aab2f6a91b64ef33526d2a7af327cdf33f5708a7
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-01-27 16:59:00 -0500

    Destructure props and add conditional text with demo login credentials if the project has a login

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,26 +12,33 @@ interface ProjectProps {
   deployed: string,
   image: string,
   description: string,
-  techUsed: string
+  techUsed: string,
+  login: boolean
 }
 
 const Project = (props: ProjectProps) => {
+  const { name, image, description, techUsed, deployed, repo, login } = props;
+
   return (
     <Row className="project-row">
       <Col sm="12" md="5">
-        <h2 className="project-heading">{props.name}</h2>
-        <img src={props.image} alt={props.name} className="project-img"></img>
+        <h2 className="project-heading">{name}</h2>
+        <img src={image} alt={name} className="project-img"></img>
       </Col>
       <Col sm="12" md="7" className="md-6 sm-12">
-        <p>{props.description}</p>
+        <p>{description}</p>
         <h3 className="project-heading">Tech Used</h3>
-        <p>{props.techUsed}</p>
-        <Button href={`${props.deployed}`} target="_blank" className="project-link">
+        <p>{techUsed}</p>
+        <Button href={`${deployed}`} target="_blank" className="project-link">
           Deployed Site
         </Button>
-        <Button href={`${props.repo}`} target="_blank" className="project-link">
+        <Button href={`${repo}`} target="_blank" className="project-link">
           <Icon icon={githubIcon} className="project-link" />
         </Button>
+
+        {login &&
+          <p>To login for a demo, use the email demo123@demo.com and password test123.</p>
+        }
       </Col>
     </Row>
   )
