b2da96f90604874fb577a97434fb5d49d1f29b23
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2021-01-27 16:58:01 -0500

    Add boolean login property to project data

--- src/assets/projectData.tsx
+++ src/assets/projectData.tsx
@@ -7,6 +7,7 @@ const projectData = [
     image: "./img/connectChorePic.jpg",
     description: "Provides incentives to kids (and adults) for getting chores done by pairing task lists with rewards and includes an integrated game for family members to compete for special prizes.",
     techUsed: "React, Redux, React-Bootstrap, MongoDb Atlas, Mongoose, Axios, Passport, Node, Express; Deployed with Heroku",
+    login: true
   },
   {
     type: "group",
@@ -15,7 +16,8 @@ const projectData = [
     deployed: "https://ascent-sobriety.herokuapp.com/",
     image: "./img/ascent.png",
     description: "A sobriety tool that helps members track their continuous and discontinuous lengths of sobriety as well as their daily mood and thoughts.",
-    techUsed: "Node.js, Express, Handlebars, MySQL, Sequelize, Passport, ApexCharts, HTML, CSS, Bootstrap, Javascirpt, jQuery"
+    techUsed: "Node.js, Express, Handlebars, MySQL, Sequelize, Passport, ApexCharts, HTML, CSS, Bootstrap, Javascirpt, jQuery",
+    login: true
   },
   {
     type: "group",
@@ -25,6 +27,7 @@ const projectData = [
     image: "./img/highCard.gif",
     description: "A relationship saver! This app helps couples, friends, and roommates fairly choose who picks dinner, and find quality dinner choices.",
     techUsed: "HTML, CSS, Materialize, JavaScript, jQuery, Moment.js, Animate.CSS, Chart.js, and multiple API calls",
+    login: false
   },
   {
     type: "individual",
@@ -33,7 +36,8 @@ const projectData = [
     deployed: "https://livesinroom29.github.io/React_Hacker_Tracker/",
     image: "./img/hacker_tracker.gif",
     description: "A searchable, sortable employee directory built with React.",
-    techUsed: "React, Node.js, JavaScript, HTML, CSS"
+    techUsed: "React, Node.js, JavaScript, HTML, CSS",
+    login: false
   },
   {
     type: "individual",
@@ -42,7 +46,8 @@ const projectData = [
     deployed: "https://still-shelf-27368.herokuapp.com/",
     image: "./img/BurgerBar_Demo.gif",
     description: "Live vicariously through your fingers! A calorie free, mess free way to dream up your favorite burger combinations and eat them all you want!",
-    techUsed: "Node.js, Express, Handlebars, MySQL, HTML, CSS, Materialize, Javascript, jQuery"
+    techUsed: "Node.js, Express, Handlebars, MySQL, HTML, CSS, Materialize, Javascript, jQuery",
+    login: false
   },
 
   {
@@ -53,6 +58,7 @@ const projectData = [
     image: "./img/weatherDash2.jpg",
     description: "Allows the user obtain weather information for any city. Retrieves data from the OpenWeather API, and displays the current weather conditions and a five day forecast.",
     techUsed: "HTML, CSS, Bulma, JavaScript, jQuery, jQueryUI, Moment.js, and multiple API calls",
+    login: false
   },
   {
     type: "individual",
@@ -62,6 +68,7 @@ const projectData = [
     image: "./img/plannerScreenShot.jpg",
     description: "A daily calendar that provides a way for the user to plan their work day; saves calendar input in local storage.",
     techUsed: "HTML, JavaScript, jQuery, Moment.js",
+    login: false
   },
   // {
   //   type: "individual",
